09c9bef9ca0edf1a2c67421bcfd3dba8638a3131
DaEtoJostka <karlovivan679@gmail.com>
2024-04-12 18:48:01 +0500

    add simple test pass exmpl

--- final_solution/solution.py
+++ final_solution/solution.py
@@ -1,11 +1,7 @@
 import typing as tp
 
-
 EntityScoreType = tp.Tuple[int, float]  # (entity_id, entity_score)
-MessageResultType = tp.List[
-    EntityScoreType
-]  # list of entity scores,
-#    for example, [(entity_id, entity_score) for entity_id, entity_score in entities_found]
+MessageResultType = tp.List[EntityScoreType]  # list of entity scores
 
 
 def score_texts(
@@ -15,11 +11,27 @@ def score_texts(
     Main function (see tests for more clarifications)
     Args:
         messages (tp.Iterable[str]): any iterable of strings (utf-8 encoded text messages)
-
     Returns:
         tp.Iterable[tp.Tuple[int, float]]: for any messages returns MessageResultType object
     -------
     Clarifications:
     >>> assert all([len(m) < 10 ** 11 for m in messages]) # all messages are shorter than 2048 characters
     """
-    raise NotImplementedError
+    results = []
+    for message in messages:
+        entities_found = []
+        # Простая логика для примера:
+        # Если в сообщении есть "Сбер", добавляем сущность с ID 150 и оценкой 3.0
+        if "Сбер" in message:
+            entities_found.append((150, 3.0))
+        # Если в сообщении есть "Тинькофф", добавляем сущность с ID 225 и оценкой 3.0
+        if "Тинькофф" in message:
+            entities_found.append((225, 3.0))
+
+        # Изменение: Проверяем, есть ли найденные сущности, прежде чем добавлять список
+        if entities_found:
+            results.append(entities_found)
+        else:
+            results.append([()])  # Добавляем пустой список, если сущностей нет
+
+    return results
